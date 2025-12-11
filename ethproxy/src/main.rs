e97e5962a15fdf6de6dded08b2e8ff199d5e7d3d
Guillaume <guillaume.thouvenin@vates.tech>
2025-12-11 18:17:22 +0100

    Cleanly quit infinite loop using ctrl-c

--- ethproxy/src/main.rs
+++ ethproxy/src/main.rs
@@ -1,4 +1,6 @@
 use std::io::{self, Write};
+use std::sync::{Arc, atomic};
+
 //use ethproxy::setup;
 
 //static VETHNAME: &str = "veth0";
@@ -8,10 +10,24 @@ fn main() {
     //let veth = setup::Veth::init(VETHNAME, VETHIP);
     //veth.create_device();
     //veth.destroy_device();
-    println!("Ctrl-C to quit");
+    let keep_looping = Arc::new(atomic::AtomicBool::new(true));
+
+    // Set handler to set keep looping to false. We need to clone
+    // it otherwise it is borrowed in the while loop. Shadow it in
+    // its own context and pass ownership in the closure.
+    {
+        let keep_looping = keep_looping.clone();
+        ctrlc::set_handler(move || {
+            // Here ownership of keep_looping is taken
+            keep_looping.store(false, atomic::Ordering::SeqCst);
+        })
+        .expect("Error setting Ctrl-C handler");
+    }
+
     let mut input = String::new();
 
-    loop {
+    println!("Ctrl-C to quit");
+    while keep_looping.load(atomic::Ordering::SeqCst) {
         print!("> ");
         io::stdout().flush().unwrap();
         io::stdin()
@@ -20,4 +36,6 @@ fn main() {
 
         print!("your input is {input}");
     }
+
+    println!("Bye!!!");
 }
