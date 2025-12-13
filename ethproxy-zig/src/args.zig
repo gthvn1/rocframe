c786b53ea4002b687c44270e11bacd59baf3f847
Guillaume <guillaume.thouvenin@vates.tech>
2025-12-13 19:36:45 +0100

    Read arguments

--- ethproxy-zig/src/args.zig
+++ ethproxy-zig/src/args.zig
@@ -1,8 +1,16 @@
 const std = @import("std");
 
-pub fn ReadArgs() void {
+const EthArgs = struct { peer_iface: [:0]const u8, socket: [:0]const u8 };
+
+pub fn ReadArgs() ?EthArgs {
     var args_it = std.process.args();
-    while (args_it.next()) |arg| {
-        std.debug.print("-> {s}\n", .{arg});
-    }
+    // We are expecting two arguments: peer_iface and socket
+    // first argument is the name of the program
+    _ = args_it.next() orelse return null;
+    const peer_iface: [:0]const u8 = args_it.next() orelse return null;
+    const socket: [:0]const u8 = args_it.next() orelse return null;
+    return .{
+        .peer_iface = peer_iface,
+        .socket = socket,
+    };
 }
--- ethproxy-zig/src/main.zig
+++ ethproxy-zig/src/main.zig
@@ -2,6 +2,11 @@ const std = @import("std");
 const a = @import("args.zig");
 
 pub fn main() !void {
-    a.ReadArgs();
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const args = a.ReadArgs() orelse {
+        std.debug.print("Usage: ethproxy <peer_iface> <socket>\n", .{});
+        std.process.exit(1);
+    };
+
+    std.debug.print("Peer Interface: {s}\n", .{args.peer_iface});
+    std.debug.print("Socket: {s}\n", .{args.socket});
 }
