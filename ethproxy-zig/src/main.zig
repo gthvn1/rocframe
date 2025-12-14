be1d997995c37a1f7edace522ab26b7668a71463
Guillaume <gthvn1@gmail.com>
2025-12-14 14:58:56 +0100

    poll for input to be ready, allow to catch ctrl-c

--- ethproxy-zig/src/main.zig
+++ ethproxy-zig/src/main.zig
@@ -1,12 +1,20 @@
 const std = @import("std");
+const posix = std.posix;
+
 const a = @import("args.zig");
 
-var quit_loop = std.atomic.Value(bool).init(false);
+var keep_looping = std.atomic.Value(bool).init(true);
 
 fn handleSigInt(sig_num: c_int) callconv(.c) void {
     _ = sig_num;
-    quit_loop.store(true, .monotonic);
+    keep_looping.store(false, .monotonic);
 }
+
+const PollAction = enum {
+    timeout_reached,
+    unknown,
+    stdin_ready,
+};
 
 pub fn main() void {
     const args = a.ReadArgs() orelse {
@@ -18,12 +26,12 @@ pub fn main() void {
     std.debug.print("Socket: {s}\n", .{args.socket});
 
     // Change the ctrl-c action
-    const action = std.posix.Sigaction{
+    const action = posix.Sigaction{
         .flags = 0,
         .handler = .{ .handler = handleSigInt },
-        .mask = std.posix.sigemptyset(),
+        .mask = posix.sigemptyset(),
     };
-    std.posix.sigaction(std.posix.SIG.INT, &action, null);
+    posix.sigaction(posix.SIG.INT, &action, null);
 
     // Read user input
     var buffer: [1024]u8 = undefined;
@@ -33,19 +41,39 @@ pub fn main() void {
     std.debug.print("Ctrl-d to end\n", .{});
     std.debug.print("> ", .{});
 
-    while (stdin.takeDelimiterExclusive('\n')) |r| {
-        // need to toss the delimiter explicitly since we're not reading it
-        stdin.toss(1);
-
-        std.debug.print("You write <{s}>\n", .{r});
-        // Should we continue
-        if (quit_loop.load(.monotonic)) {
-            std.debug.print("bye\n", .{});
-            break;
+    while (keep_looping.load(.monotonic)) {
+        switch (wait(posix.STDIN_FILENO)) {
+            .timeout_reached => continue,
+            .stdin_ready => {
+                if (stdin.takeDelimiterExclusive('\n')) |r| {
+                    // need to toss the delimiter explicitly since we're not
+                    // reading it
+                    stdin.toss(1);
+                    std.debug.print("You write <{s}>\n", .{r});
+                    std.debug.print("> ", .{});
+                } else |_| {
+                    std.debug.print("end\n", .{});
+                    std.process.exit(1);
+                }
+            },
+            .unknown => {
+                std.debug.print("What happens?\n", .{});
+                continue;
+            },
         }
-        std.debug.print("> ", .{});
-    } else |_| {
-        std.debug.print("end\n", .{});
-        std.process.exit(1);
     }
+
+    std.debug.print("bye\n", .{});
+}
+
+fn wait(stdin: std.posix.fd_t) PollAction {
+    var fds = [_]posix.pollfd{
+        .{ .fd = stdin, .events = posix.POLL.IN, .revents = 0 },
+    };
+
+    // Wait 100ms
+    const n = posix.poll(&fds, 100) catch unreachable;
+    if (n == 0) return .timeout_reached;
+    if ((fds[0].revents & posix.POLL.IN) != 0) return .stdin_ready;
+    return .unknown;
 }
